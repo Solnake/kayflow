7c1a307e0b49fd63af55f6fbe560f04a36868a0c
Solnake <solnake@gmail.com>
2015-07-23 15:01:23 +0300

    #6
    Signed-off-by: Solnake <solnake@gmail.com>

--- Kayflow.Web/Controls/Content/TickerControl.ascx
+++ Kayflow.Web/Controls/Content/TickerControl.ascx
@@ -1,1 +1,2 @@
 ﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="TickerControl.ascx.cs" Inherits="Controls_Content_TickerControl" %>
+<marquee scrollamount="3"><%=GetMessagesArray() %></marquee>
--- Kayflow.Web/Controls/Content/TickerControl.ascx.cs
+++ Kayflow.Web/Controls/Content/TickerControl.ascx.cs
@@ -1,11 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+using System.Text;
+using Kayflow.Manager;
 
 public partial class Controls_Content_TickerControl : BaseControl
 {
-    
+    public string GetMessagesArray()
+    {
+        if (CurrentOffice == null)
+            return String.Empty;
+
+        StringBuilder sb = new StringBuilder();
+        var list = CreateManager<CompanyMessageManager>().GetByOffice(CurrentOffice.ID);
+        if (list.Count==0)
+            return String.Empty;
+
+        sb.Append("&nbsp&#149;&nbsp;&nbsp;");
+        foreach (var item in list)
+        {
+            sb.Append(escapeHTML(item.MessageText) + "&nbsp;&nbsp;&#149;&nbsp;&nbsp;");
+        }
+
+        return sb.ToString();
+    }
+
+
+
+    private string escapeHTML(string str)
+    {
+        return str
+            .Replace("<", "&lt;")
+            .Replace(">", "&gt;")
+            .Replace("\r", " ")
+            .Replace("\n", " ")
+            .Replace("\"", "&quot;");
+    }
 }
